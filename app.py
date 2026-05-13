c359f932a94abc956054dab6c1f68939af000e23
karolína svobodova <svobodova.karolina@student.gymzr.cz>
2026-05-13 22:35:39 +0200

    nahodnycitat

--- app.py
+++ app.py
@@ -1,26 +1,14 @@
-from flask import Flask, render_template, request
-from datetime import datetime
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def index():
-	#aktuální datum
-	date = datetime.now().strftime("%d. %m. %Y")
+    with open("citaty.txt", encoding="utf-8") as soubor:
+        citaty = [line.strip() for line in soubor if line.strip()]
 
-	citat = [ "„Neberte život příliš vážně, stejně z něho nevyváznete živí.“ Elbert Hubbard",
-          "„Největší sláva není v tom, že nikdy neselžeme, ale v tom, že se vždy zvedneme.“ Konfucius",
-          "„Největší dobrodružství, které můžete podniknout, je žít život svých snů.“ Oprah Winfrey",
-          "„Největší překážkou k úspěchu je strach z neúspěchu.“ Sven Goran Eriksson",
-          "„Největší bohatství je zdraví.“ Virgil",
-          "„Bez hudby by byl život chybou.“ Friedrich Nietzsche",
-          "„V životě nevěř tomu, kdo tě klame, ale nezklam toho, kdo ti věří.“ Neznámý autor",
-          "„Zeptáš-li se, budeš 5 minut vypadat jako blbec. Nezeptáš-li se, budeš blbcem po celý život.“ Čínské přísloví",
-          "„Zatímco ztrácíme svůj čas váháním a odkládáním, život utíká.“ Seneca",
-          "„Pamatuj, že i ta nejtěžší hodina ve tvém životě, má jen 60 minut.“ Sofoklés"]
+    return render_template("index.html", citaty=citaty)
 
-
-	return render_template("index.html", date=date, citat=citat)
-
-if __name__=="__main__":
-	app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
