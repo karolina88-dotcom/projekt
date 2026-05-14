f9f258d7e9c491e0aad85a7e40159e573ab2a9b6
karolína svobodova <svobodova.karolina@student.gymzr.cz>
2026-05-14 07:58:04 +0200

    nahoda

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+
+    return render_template("index.html", citat="")
+
+@app.route("/citat")
+def citat():
     with open("citaty.txt", encoding="utf-8") as soubor:
         citaty = [line.strip() for line in soubor if line.strip()]
+    
+    index=random.randint(0, len(citaty) - 1)
+    citat = citaty[index]
 
-    return render_template("index.html", citaty=citaty)
+    return render_template("index.html", citat=citat)
 
 if __name__ == "__main__":
     app.run(debug=True)
