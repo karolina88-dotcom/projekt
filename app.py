612906d9b8282bd6384bf4df3dd7c804e98c126a
Svobodová Karolína <svobodova.karolina@student.gymzr.cz>
2026-05-14 15:15:13 +0200

    dodelatpost

--- app.py
+++ app.py
@@ -16,6 +16,25 @@ def citat():
     index=random.randint(0, len(citaty) - 1)
     citat = citaty[index]
 
+    if request.method == "POST":
+        MOTIVACE ="motivace"
+        HUMOR = "humor"
+        FILOZOFIE = "filozofie"
+
+
+        if tema==MOTIVACE:
+            return render_template(
+                citat="„Budoucnost patří těm, kdo věří svým krásným snům.“ Eleanor Roosevelt")
+        elif tema==HUMOR:
+            return render_template(
+                citat="„Neberte život příliš vážně, stejně z něho nevyváznete živí.“ Elbert Hubbard")
+        elif tema==FILOZOFIE:
+            return render_template(
+                citat="„Nemám slov, abych vyjádřil, co cítím při čtení knih. Trávím spoustu času čtením, protože to velmi miluji. Dokážu se v té knížce úplně ztratit a zapomenout, kde jsem. Filozofie je můj oblíbený předmět. To, co je skvělé na čtení, je, že na jednom řádku můžete narazit na něco, co jste se snažili vyjádřit celý život, nebo něco, na co jste se snažili přijít celý život.“ Michael Jackson")
+
+        
+
+
     return render_template("index.html", citat=citat)
 
 if __name__ == "__main__":
