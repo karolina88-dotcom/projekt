a0d2ff773e50c12d0e045df611f73b01587a0c91
karolína svobodova <svobodova.karolina@student.gymzr.cz>
2026-05-17 16:14:04 +0200

    hotovo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ def index():
 
     return render_template("index.html", citat="")
 
-@app.route("/citat")
+@app.route("/citat", methods=["GET", "POST"])
 def citat():
     with open("citaty.txt", encoding="utf-8") as soubor:
         citaty = [line.strip() for line in soubor if line.strip()]
@@ -17,6 +17,7 @@ def citat():
     citat = citaty[index]
 
     if request.method == "POST":
+        tema = request.form.get("tema")
         MOTIVACE ="motivace"
         HUMOR = "humor"
         FILOZOFIE = "filozofie"
@@ -24,18 +25,21 @@ def citat():
 
         if tema==MOTIVACE:
             return render_template(
+                "citat.html",
                 citat="„Budoucnost patří těm, kdo věří svým krásným snům.“ Eleanor Roosevelt")
         elif tema==HUMOR:
             return render_template(
+                "citat.html",
                 citat="„Neberte život příliš vážně, stejně z něho nevyváznete živí.“ Elbert Hubbard")
         elif tema==FILOZOFIE:
             return render_template(
+                "citat.html",
                 citat="„Nemám slov, abych vyjádřil, co cítím při čtení knih. Trávím spoustu času čtením, protože to velmi miluji. Dokážu se v té knížce úplně ztratit a zapomenout, kde jsem. Filozofie je můj oblíbený předmět. To, co je skvělé na čtení, je, že na jednom řádku můžete narazit na něco, co jste se snažili vyjádřit celý život, nebo něco, na co jste se snažili přijít celý život.“ Michael Jackson")
 
         
 
 
-    return render_template("index.html", citat=citat)
+    return render_template("citat.html", citat=citat)
 
 if __name__ == "__main__":
     app.run(debug=True)
